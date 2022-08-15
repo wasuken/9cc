a2966e58cc63c4f1044a5234c58b5256f55d52a0
wasuken <wevorence@gmail.com>
2022-08-15 23:06:17 +0900

    エラー関数追加

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 
 assert(){
   expected="$1"
-  input="$1"
+  input="$2"
 
   ./9cc "$input" > tmp.s
-  cc -o tmp tmp.s
+  cc -o tmp tmp.s -Wa,--noexecstac
   ./tmp
   actual="$?"
 
@@ -17,8 +17,10 @@ assert(){
   fi
 }
 
-assert 0 0
-assert 42 42
+# assert 0 0
+# assert 42 42
 assert 21 "5+20-4"
+assert 41 "12 + 34 - 5"
+assert 41 " 12 + 34 - 5 "
 
 echo OK
